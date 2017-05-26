8da1a267ab4db7ffe0c8d3c806125b315fd8b87c
codejunkie <user@codejunkie.ie>
2017-05-26 03:29:38 +0100

    Initial commit
    all files, frost is up

---
+++ web/js/index.js
@@ -0,0 +1,41 @@
+$(function(){
+	// $.ajax('https://api.rss2json.com/v1/api.json?rss_url=http://bwe.ng/rss', {
+	// 	'dataType': 'json',
+	// 	success: (d) => {
+	// 		let updateHtml = '', m = 0
+	// 		if (d.items.length > 0) {
+	// 			while (m < 6) {
+	// 				updateHtml += `<div class="col-md-4 col-sm-6 col-xs-12" style="height: 150px;margin-bottom: 80px;">
+	// 					<a href="${d.items[m].link}" class="fh5co-project-item image-popup" target="_blank">
+	// 						<div class="fh5co-text">
+	// 							<h2>${d.items[m].title}</h2>
+	// 							<p>${d.items[m].description}</p>
+	// 						</div>
+	// 					</a>
+	// 				</div>
+	// 				`
+	// 				m++
+	// 			}	
+	// 			$('.updates-container').html(updateHtml)
+	// 			// console.log(d.items[0])
+	// // 			console.log(updateHtml)
+	// $('.fh5co-text p').succinct({size: 100,  omission: '&rarr;'})
+	// 		}
+	// 	}
+	// })
+
+
+    // $('#fh5co-hero').vegas({
+    //     slides: [
+    //         { src: '/images/slider/1.jpg' },
+    //         { src: 'images/slider/2.jpg' },
+    //         { src: 'images/slider/3.jpg' },
+    //         { src: 'images/slider/4.jpg' },
+    //         { src: 'images/slider/5.jpg' },
+    //         { src: 'images/slider/6.jpg' },
+    //         { src: 'images/slider/7.jpg' }
+    //         ],
+    //     overlay: '/node_modules/vegas/dist/overlays/05.png'
+    // });
+
+});
